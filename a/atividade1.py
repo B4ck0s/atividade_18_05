d76eebe407ba9f05dd72506a18558400ca4a9d94
caio sales <160229976+B4ck0s@users.noreply.github.com>
2025-05-19 12:18:04 -0300

    Add files via upload

---
+++ a/atividade1.py
@@ -0,0 +1,6 @@
+def calcular_numeros(n1,n2):
+    soma = n1+n2
+    return soma
+n1 = 11
+n2 = 94
+print ("resultado", calcular_numeros(n1,n2))
---
+++ a/atividade2.py
@@ -0,0 +1,9 @@
+def numero (n):
+    if n%2 == 0:
+     return "par"
+    else:
+       return "impar"
+    
+n = 100,5
+print("o número é:", numero(n))
+
